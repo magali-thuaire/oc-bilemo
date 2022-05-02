19517534f1adfc908f31cc48a02cf274a63beb01
Magali <magali.thuaire@gmail.com>
2022-05-02 16:43:41 +0200

    📦 Added bundle Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
